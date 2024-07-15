9b55b37f3b1ef5629b9005cfa906e9f5e0c76f62
Prekshya <prekshya.dali@bajratechnologies.com>
2024-07-15 10:39:42 +0545

    feat: add MAUI UI elements practice files

---
+++ MauiApp1/Views/UIPracticePage.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiApp1.Views;
+using MauiApp1.ViewModels;
+
+public partial class UIPracticePage : ContentPage
+{
+	public UIPracticePage()
+	{
+		InitializeComponent();
+		BindingContext = new FairyTaleViewModel();
+	
+	}
+
+}
