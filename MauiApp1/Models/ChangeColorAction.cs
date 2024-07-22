1ff8ed8012fd9550726cebd921463de2f2ade945
Prekshya <prekshya.dali@bajratechnologies.com>
2024-07-22 17:51:11 +0545

    feat: add MAUI practice file: enter and exit trigger

--- MauiApp1/Models/ChangeColorAction.cs
+++ MauiApp1/Models/ChangeColorAction.cs
@@ -1,15 +1,42 @@
-﻿using Microsoft.Maui.Controls;
+﻿//using Microsoft.Maui.Controls;
+//using Microsoft.Maui.Graphics;
+
+//namespace MauiApp1.Models
+//{
+//    public class ChangeColorAction : TriggerAction<Entry>
+//    {
+
+//        protected override void Invoke(Entry entry)
+//        {
+//            entry.TextColor = Colors.Red;
+//            entry.BackgroundColor = Colors.Green;
+//        }
+//    }
+//}
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 
 namespace MauiApp1.Models
 {
     public class ChangeColorAction : TriggerAction<Entry>
     {
+        public Color FocusedTextColor { get; set; } = Colors.Red;
+        public Color FocusedBackgroundColor { get; set; } = Colors.Green;
+        public Color UnfocusedTextColor { get; set; } = Colors.Black;
+        public Color UnfocusedBackgroundColor { get; set; } = Colors.Transparent;
 
         protected override void Invoke(Entry entry)
         {
-            //entry.TextColor = Colors.Red;
-            //entry.BackgroundColor = Colors.Green;
+            if (entry.IsFocused)
+            {
+                entry.TextColor = FocusedTextColor;
+                entry.BackgroundColor = FocusedBackgroundColor;
+            }
+            else
+            {
+                entry.TextColor = UnfocusedTextColor;
+                entry.BackgroundColor = UnfocusedBackgroundColor;
+            }
         }
     }
 }
