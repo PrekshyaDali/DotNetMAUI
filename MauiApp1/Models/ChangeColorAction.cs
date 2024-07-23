e5a8c00d21b6d9b452f4e4820d42e20cc4227d10
Prekshya <prekshya.dali@bajratechnologies.com>
2024-07-23 10:44:50 +0545

    feat: add MAUI practice file: state trigger

--- MauiApp1/Models/ChangeColorAction.cs
+++ MauiApp1/Models/ChangeColorAction.cs
@@ -1,19 +1,4 @@
-﻿//using Microsoft.Maui.Controls;
-//using Microsoft.Maui.Graphics;
-
-//namespace MauiApp1.Models
-//{
-//    public class ChangeColorAction : TriggerAction<Entry>
-//    {
-
-//        protected override void Invoke(Entry entry)
-//        {
-//            entry.TextColor = Colors.Red;
-//            entry.BackgroundColor = Colors.Green;
-//        }
-//    }
-//}
-using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 
 namespace MauiApp1.Models
